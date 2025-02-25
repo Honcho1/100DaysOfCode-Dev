98f31be8cb5716d7f83add03c36d0affa1064b26
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-02-25 13:37:09 +0100

    Add the JavaScript code containing the questions

---
+++ Day2/script.js
@@ -0,0 +1,38 @@
+const quizData = [
+  {
+    question: "Which planet is known as the 'Red Planet'?",
+    options: ["Venus", "Mars", "Jupiter", "Saturn"],
+    answer: "Mars",
+  },
+  {
+    question:
+      "What programming language is primarily used for styling web pages?",
+    options: ["JavaScript", "Python", "CSS", "HTML"],
+    answer: "CSS",
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    options: [
+      "Vincent van Gogh",
+      "Pablo Picasso",
+      "Michelangelo",
+      "Leonardo da Vinci",
+    ],
+    answer: "Leonardo da Vinci",
+  },
+  {
+    question: "Which of these is NOT a JavaScript data type?",
+    options: ["Boolean", "Integer", "String", "Object"],
+    answer: "Integer",
+  },
+  {
+    question: "What does the acronym DOM stand for in web development?",
+    options: [
+      "Document Object Model",
+      "Digital Ordinance Management",
+      "Data Object Manipulation",
+      "Document Orientation Method",
+    ],
+    answer: "Document Object Model",
+  },
+];
